8e1df8a89c8b0ecc4848effc28288237dbe98094
Owen Abbott <ocabbott@gmail.com>
2020-01-27 03:22:41 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,11 @@
 def find_element_index(array, value_to_find)
-  array.
+  return array.max()
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.max()
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.min()
 end
